70d14fec1a74a949abd715dfab0802d583054b0a
Piotr Szulc <szulo123@gmail.com>
2012-11-13 00:06:27 +0100

    Started solving subs

---
+++ subs/subs.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $in = [];
+while (<>) {
+   chomp;
+   next unless $_;
+   push @$in, $_;
+}
+
+print join ' ', positions($in->[0], $in->[1]);
+
+sub positions {
+   my $string = shift;
+   my $subs   = shift;
+
+   my @ret = ();
+   while ($string =~ m/$subs/g) {
+      push @ret, ($-[0] + 1);
+   }
+   return @ret;
+}

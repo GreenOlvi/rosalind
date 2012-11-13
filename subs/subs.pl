3879a8a6594f90ac75a64f84aa076c66e006f852
Piotr Szulc <szulo123@gmail.com>
2012-11-13 22:03:10 +0100

    Solved subs

--- subs/subs.pl
+++ subs/subs.pl
@@ -11,13 +11,14 @@ while (<>) {
 }
 
 print join ' ', positions($in->[0], $in->[1]);
+print "\n";
 
 sub positions {
    my $string = shift;
    my $subs   = shift;
 
    my @ret = ();
-   while ($string =~ m/$subs/g) {
+   while ($string =~ m/(?=$subs)/g) {
       push @ret, ($-[0] + 1);
    }
    return @ret;

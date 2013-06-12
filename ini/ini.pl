dff3fa1ee0db87e807ec204ed71974d409ce4a0e
Piotr Szulc <szulo123@gmail.com>
2013-06-12 23:02:50 +0200

    Solved ini

---
+++ ini/ini.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use v5.10;
+
+while (<>) {
+   chomp;
+   my $count = get_count($_);
+   printf "%d %d %d %d", $count->('A'), $count->('C'), $count->('G'), $count->('T');
+}
+
+exit 0;
+
+use Data::Dumper;
+sub get_count {
+   my $seq = shift;
+   my $c = {};
+   foreach (split //, $seq) {
+      $c->{$_}++;
+   }
+   return sub { $c->{ shift() } };
+}
